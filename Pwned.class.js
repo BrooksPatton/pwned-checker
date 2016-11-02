a82f4a71c3d42e0334dc69413b1eefcd9ce59648
Brooks Patton <brooks@brookzerker.com>
2016-11-02 13:17:29 -0600

    fixed standard eslint errors

--- Pwned.class.js
+++ Pwned.class.js
@@ -1,25 +1,25 @@
 /* eslint no-param-reassign: */
 
-const moment = require('moment');
+const moment = require('moment')
 
-function clean(str) {
-  str = str.replace(/<a href="/g, '(');
-  str = str.replace(/" target="_blank" rel="noopener">/g, ') ');
-  str = str.replace(/<\/a>/g, '');
-  str = str.replace(/<em>/g, '');
-  str = str.replace(/<\/em>/g, '');
-  str = str.replace(/&quot;/g, '"');
+function clean (str) {
+  str = str.replace(/<a href="/g, '(')
+  str = str.replace(/" target="_blank" rel="noopener">/g, ') ')
+  str = str.replace(/<\/a>/g, '')
+  str = str.replace(/<em>/g, '')
+  str = str.replace(/<\/em>/g, '')
+  str = str.replace(/&quot/g, '"')
 
-  return str;
+  return str
 }
 
 module.exports = class Pwned {
-  constructor(pwn) {
-    this.host = pwn.Domain;
-    this.breachDate = moment(pwn.BreachDate).fromNow();
-    this.isPasswordCompromised = JSON.parse(pwn.DataClasses.includes('Passwords'));
-    this.isPasswordHintsCompromised = JSON.parse(pwn.DataClasses.includes('Password hints'));
-    this.description = clean(pwn.Description);
+  constructor (pwn) {
+    this.host = pwn.Domain
+    this.breachDate = moment(pwn.BreachDate).fromNow()
+    this.isPasswordCompromised = JSON.parse(pwn.DataClasses.includes('Passwords'))
+    this.isPasswordHintsCompromised = JSON.parse(pwn.DataClasses.includes('Password hints'))
+    this.description = clean(pwn.Description)
   }
 
-};
+}
